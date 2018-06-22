a229b6b4ab0f61d0138fe26df0dc632a5774741d
Barak2222 <barakcohen54@gmail.com>
2018-06-22 17:33:33 +0300

    Added RollerCoaster class with grass

--- Ex4/src/edu/cg/Main.java
+++ Ex4/src/edu/cg/Main.java
@@ -23,7 +23,7 @@ import edu.cg.models.*;
 
 public class Main {
 
-	static IRenderable[] models = {new Locomotive()};
+	static IRenderable[] models = {new RollerCoaster(), new Locomotive()};
 	static Point prevMouse;
 	static int currentModel;
 	static Frame frame;
